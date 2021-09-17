36566162718c1f3aabb829490fc2a5510d65dc06
Kitty Hsieh <kitty_hsieh@wistron.com>
2021-09-17 14:03:01 +0800

    At Last! We Actually Write Some Application Code! P26~30 終於!我真的開始寫應用程式了!
    Versions Note：
    因為版本差異會出現
    ```
    TypeError: view must be a callable or a list/tuple in the case of include().
    ```
    此時請更改 home_page 為 function

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,9 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-        # 將 URL 解析為網站的根目錄("/")，指向我們製作的特定 view 函式
-        # view 函式會回傳 HTML
