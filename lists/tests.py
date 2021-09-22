94f1005061038b5dc81e66ddccd70d4cbaf9251c
Kitty Hsieh <kitty_hsieh@wistron.com>
2021-09-22 13:27:15 +0800

    Saving the POST to the Database 重構之前

--- lists/tests.py
+++ lists/tests.py
@@ -15,18 +15,11 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertIn('1: A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
