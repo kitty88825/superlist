b800cc58fc767f1721c5e452f35ee6883498a602
Kitty Hsieh <kitty_hsieh@wistron.com>
2021-09-22 17:42:23 +0800

    A Final Refactor Using URL includes

--- lists/urls.py
+++ lists/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.conf.urls import include
 from django.urls import path
 
 from lists import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/', include('lists.urls')),
-    path('admin/', admin.site.urls),
+    path('new/', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item/', views.add_item, name='add_item'),
 ]
