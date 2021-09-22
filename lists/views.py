f74233c2657d67db2e96205b70554eb3ceb8809a
Kitty Hsieh <kitty_hsieh@wistron.com>
2021-09-22 17:20:10 +0800

    One More View to Handle Adding Items to an Existing List

--- lists/views.py
+++ lists/views.py
@@ -16,3 +16,9 @@ def new_list(request):
 def view_list(request, list_id):
     items = Item.objects.filter(list_id=list_id)
     return render(request, 'list.html', {'items': items})
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
