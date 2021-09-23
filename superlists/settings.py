291a14786d77634b2331f186a8e8ac4e58e04d90
Kitty Hsieh <kitty_hsieh@wistron.com>
2021-09-23 13:27:56 +0800

    What We Glossed Over: collectstatic and Other Static Directories

--- superlists/settings.py
+++ superlists/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = PurePosixPath(BASE_DIR).joinpath('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
