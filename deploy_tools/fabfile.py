974e2cf47c6f1d5e3ade5b04afb72fbd94af3d8e
Kitty <kitty40632@gmail.com>
2021-09-27 17:06:02 +0800

    Breakdown of a Fabric Script for Our Deployment

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,18 @@
+import random
+
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+
+REPO_URL = 'https://github.com/kitty88825/superlist.git'
+
+
+def deploy():
+    site_folder = f'/home/{env.user}/site/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
