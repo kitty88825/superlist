492fe2d35da49d9aa32cdb82e2e74ea151773784
Kitty <kitty40632@gmail.com>
2021-09-27 17:11:28 +0800

    Pulling Down Our Source Code with Git

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -16,3 +16,13 @@ def deploy():
         _create_or_update_dotenv()
         _update_static_files()
         _update_database()
+
+
+def _get_latest_source():
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {REPO_URL} .')
+
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
