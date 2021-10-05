4933d905f17df057874462ad45b392f0c4da07f4
Kitty <kitty40632@gmail.com>
2021-10-05 13:57:01 +0800

    Using get_absolute_url for Redirects 使用 get_absolute_url 來重新導向(第十章 p199)
    將 views.py 內寫死的 URL 移除
    在 models.py 設定 get_absolute_url
    並將 views.py 中的導向用 reverse 重新撰寫

--- lists/views.py
+++ lists/views.py
@@ -18,7 +18,7 @@ def new_list(request):
         error = "You can't have an empty list item."
         return render(request, 'home.html', {'error': error})
 
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
 
 
 def view_list(request, list_id):
@@ -32,7 +32,7 @@ def view_list(request, list_id):
             )
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
             item.delete()
             error = "You can't have an empty list item."
