7e9deadee22209475b45e30127cff50c4dc4b0e7
Kitty <kitty40632@gmail.com>
2021-10-05 14:43:33 +0800

    Switching to a Django ModelForm 切換到 Django ModelForm(第十一章 p206~207)

--- lists/forms.py
+++ lists/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 
+from .models import Item
 
-class ItemForm(forms.Form):
-    item_text = forms.CharField(
-        widget=forms.fields.TextInput(attrs={
-            'placeholder': 'Enter a to-do item',
-            'class': 'form-control input-lg',
-        }),
-    )
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
