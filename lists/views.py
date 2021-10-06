c30e0b348d0ac67edde7a588aa188b7de6b46673
Kitty <kitty40632@gmail.com>
2021-10-06 14:06:44 +0800

    A Helper Method for Several Short Tests 供一些測試使用的協助方法(第十一章 p217~219)
    要 view_list 的 POST method 之前先撰寫測試
    再進行修改 views.py
    在此動作中修復所有的單元測試
    但此時的 FT 測試含有錯誤
    此錯誤為 HTML5 自帶的驗證

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 
 from .forms import ItemForm
@@ -20,24 +19,13 @@ def new_list(request):
 
 
 def view_list(request, list_id):
-    form = ItemForm()
     list_ = List.objects.get(id=list_id)
-    error = None
-    if request.method == 'POST':
-        try:
-            item = Item.objects.create(
-                text=request.POST['text'],
-                list=list_,
-            )
-            item.full_clean()
-            item.save()
-            return redirect(list_)
-        except ValidationError:
-            item.delete()
-            error = "You can't have an empty list item."
+    form = ItemForm()
 
-    return render(request, 'list.html', {
-        'list': list_,
-        'form': form,
-        'error': error,
-    })
+    if request.method == 'POST':
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
+            return redirect(list_)
+
+    return render(request, 'list.html', {'list': list_, 'form': form})
