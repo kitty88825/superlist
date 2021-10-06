cd0e816a47d4cb0ffab2d259608ec88d236a3a22
Kitty <kitty40632@gmail.com>
2021-10-06 15:22:28 +0800

    Another FT for Duplicate Items 另一個重複項目的 FT (第十二章 p224)
    新增新的 FT
    測試使用者不可以輸入重複的項目

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -44,3 +44,20 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        # Edith 前往首頁，開始編輯一個新的清單
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # 她不小心試著輸入一個重複的項目
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # 她看到一個有用的錯誤訊息
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You've already got this in your list.",
+        ))
