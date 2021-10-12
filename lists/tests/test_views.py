ec174103d317849f75b8dcdc483304512d1ad01e
Kitty <kitty40632@gmail.com>
2021-10-12 13:39:48 +0800

    Using the Existing List Item Form in the List View 在清單 View 中使用既有的清單項目表單(第十二章 p234~237)

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.utils.html import escape
 
 from ..models import Item, List
-from ..forms import ItemForm, EMPTY_ITEM_ERROR
+from ..forms import ItemForm, EMPTY_ITEM_ERROR, ExistingListItemForm
 
 
 class HomePageTest(TestCase):
@@ -73,7 +73,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def post_invalid_input(self):
@@ -97,14 +97,13 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         '''如果驗證失敗，將表單物件傳給範本'''
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         '''如果驗證失敗，回應錯誤訊息'''
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         _ = Item.objects.create(list=list1, text='textey')
