4be3e22b696c9aa65b0be83e7fedb09a011aae3f
Kitty <kitty40632@gmail.com>
2021-10-12 16:06:07 +0800

    Remove view_list function

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView, CreateView
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 
 from .forms import ItemForm, ExistingListItemForm
 from .models import List
@@ -27,16 +27,3 @@ class ViewAndAddToList(DetailView, CreateView):
     def get_form(self):
         self.object = self.get_object()
         return self.form_class(for_list=self.object, data=self.request.POST)
-
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ExistingListItemForm(for_list=list_)
-
-    if request.method == 'POST':
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(list_)
-
-    return render(request, 'list.html', {'list': list_, 'form': form})
