771f6b49ab95c27aeb92a61a9cd6a89a46fdcddd
Kitty Hsieh <kitty40632@gmail.com>
2021-10-25 19:18:42 +0800

    Using Our Auth Backend in the Login View

--- superlists/settings.py
+++ superlists/settings.py
@@ -60,6 +60,9 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 # https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#substituting-a-custom-user-model
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
