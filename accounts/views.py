7fde8e939b4c2b5e98ccbb6508a18b32e21544ea
Kitty Hsieh <kitty40632@gmail.com>
2021-10-25 19:29:08 +0800

    Using mock.return_value

--- accounts/views.py
+++ accounts/views.py
@@ -27,5 +27,6 @@ def send_login_email(request):
 
 
 def login(request):
-    auth.authenticate(uid=request.GET.get('token'))
+    user = auth.authenticate(uid=request.GET.get('token'))
+    auth.login(request, user)
     return redirect('/')
