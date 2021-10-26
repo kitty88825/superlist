80e98decb30a4a04150aff109d948ef2463a961f
Kitty Hsieh <kitty40632@gmail.com>
2021-10-26 13:42:41 +0800

    修正 find elements to find element

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -45,7 +45,7 @@ class LoginTest(FunctionalTest):
 
         # 她已登入!
         self.wait_for(
-            lambda: self.browser.find_elements_by_link_text('Log out'),
+            lambda: self.browser.find_element_by_link_text('Log out'),
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
