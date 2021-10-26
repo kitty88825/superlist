422a9bc79931215f9ee402181e7ccb35f97f29a8
Kitty Hsieh <kitty40632@gmail.com>
2021-10-26 15:52:53 +0800

    Using Our New Environment Variable, and Saving It to .env

--- superlists/settings.py
+++ superlists/settings.py
@@ -147,3 +147,10 @@ STATIC_ROOT = PurePosixPath(BASE_DIR).joinpath('static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
