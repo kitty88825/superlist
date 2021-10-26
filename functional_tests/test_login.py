c05a7a1b5f401d21dd109935db001a436dfd1ad5
Kitty Hsieh <kitty40632@gmail.com>
2021-10-26 17:16:53 +0800

    Checking That It Works
    確認它可以動作(第十七章 p325~326)
    在 base FT 程式當中加入測試登入登出的測試
    並修改繼承 base FT 的 test_login 測試底下的重複程式
    最後測試我們在 my_lists 中使用 session 的登入登出

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -44,18 +44,10 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # 她已登入!
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out'),
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # 現在她要登出
         self.browser.find_element_by_link_text('Log out').click()
 
         # 她已經登出
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
