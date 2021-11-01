10ea8f8310b2529349952b70ee212cc8ee99838b
Kitty Hsieh <kitty40632@gmail.com>
2021-11-01 13:56:25 +0800

    Getting the FT to Run the Management Command on the Server
    在伺服器讓 FT 執行管理指令（第十七章 p334~p338)

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,8 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from webdriver_manager.firefox import GeckoDriverManager
 
+from .server_tools import reset_database
+
 
 MAX_WAIT = 10
 
@@ -31,6 +33,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = f'http://{self.staging_server}'
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
